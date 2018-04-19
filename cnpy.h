d8efb27c81701f1c03ab675819cf7a7b91f880d5
hangtingchen <465244243@qq.com>
2018-04-19 12:18:19 +0800

    Update cnpy.h

--- cnpy.h
+++ cnpy.h
@@ -68,8 +68,6 @@ namespace cnpy {
     void parse_npy_header(FILE* fp,size_t& word_size, std::vector<size_t>& shape, bool& fortran_order);
     void parse_npy_header(unsigned char* buffer,size_t& word_size, std::vector<size_t>& shape, bool& fortran_order);
     void parse_zip_footer(FILE* fp, uint16_t& nrecs, size_t& global_header_size, size_t& global_header_offset);
-    npz_t npz_load(std::string fname);
-    NpyArray npz_load(std::string fname, std::string varname);
     NpyArray npy_load(std::string fname);
 
     template<typename T> std::vector<char>& operator+=(std::vector<char>& lhs, const T rhs) {
